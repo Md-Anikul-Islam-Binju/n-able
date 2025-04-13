c3e8d471938d90f2d193c125b220dba11a800d3a
Md-Anikul-Islam <anikul.islam01@northsouth.edu>
2025-04-13 15:06:49 +0600

    first commit

--- resources/js/Pages/Index.vue
+++ resources/js/Pages/Index.vue
@@ -328,21 +328,6 @@
                 <div class="slide"  v-for="(company, index) in companys" :key="index">
                     <img :src="baseUrl + '/images/company/' + company.image" height="100" width="250" alt="">
                 </div>
-<!--                <div class="slide"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/2.png" height="100" width="250" alt=""></div>-->
-<!--                <div class="slide"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/3.png" height="100" width="250" alt=""></div>-->
-<!--                <div class="slide"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/4.png" height="100" width="250" alt=""></div>-->
-<!--                <div class="slide"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/5.png" height="100" width="250" alt=""></div>-->
-<!--                <div class="slide"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/6.png" height="100" width="250" alt=""></div>-->
-<!--                <div class="slide"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/7.png" height="100" width="250" alt=""></div>-->
-
-<!--                &lt;!&ndash; Duplicate for smooth loop &ndash;&gt;-->
-<!--                <div class="slide"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/1.png" height="100" width="250" alt=""></div>-->
-<!--                <div class="slide"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/2.png" height="100" width="250" alt=""></div>-->
-<!--                <div class="slide"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/3.png" height="100" width="250" alt=""></div>-->
-<!--                <div class="slide"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/4.png" height="100" width="250" alt=""></div>-->
-<!--                <div class="slide"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/5.png" height="100" width="250" alt=""></div>-->
-<!--                <div class="slide"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/6.png" height="100" width="250" alt=""></div>-->
-<!--                <div class="slide"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/7.png" height="100" width="250" alt=""></div>-->
             </div>
         </div>
     </div>
@@ -371,8 +356,6 @@
 
 
     <!-- News & Events -->
-
-
     <div class="container">
         <div class="d-flix justify-content-center text-center" >
             <h1>News & Events</h1>
